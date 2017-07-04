367be2b3c34c2af2c556457befc8548da6b88890
reg368 <reg368@gmail.com>
2017-07-04 21:30:01 +0800

    加入專案檔案。

---
+++ digitallearningback/Controllers/LoginController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using digitallearningback.Models;
+
+namespace digitallearningback.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        // POST : check login
+        [HttpPost]
+        public ActionResult Login(User user) {
+
+            if (ModelState.IsValid){
+                return RedirectToAction("Index", "Home");
+            }
+            else{
+                ModelState.AddModelError(string.Empty, "Student Name already exists.");
+                return View(user);
+            }
+
+        }
+    }
+}
---
+++ digitallearningback/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="digitallearningback.MvcApplication" Language="C#" %>
