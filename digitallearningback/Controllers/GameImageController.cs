2e38fccbbc4a014b8976c9f5dbfb2520b5fb4e2c
reg368 <reg368@gmail.com>
2017-07-11 21:45:47 +0800

    add after login index page

---
+++ digitallearningback/Controllers/GameImageController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace digitallearningback.Controllers
+{
+    public class GameImageController : Controller
+    {
+        // GET: GameImage
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
