8e715f7dfe873961d0d691fce743ab1cadfbde3c
reg368 <reg368@gmail.com>
2017-08-13 13:38:32 +0800

    add CSharpRazorViewEngine

---
+++ digitallearningback/App_Start/ViewEngineConfig.cs
@@ -0,0 +1,57 @@
+﻿using System.Web.Mvc;
+
+namespace digitallearningback.App_Start
+{
+    public class ViewEngineConfig
+    {
+        public static void Register(ViewEngineCollection viewEngines)
+        {
+            //20170811-  調整ViewEngine 順序 先找.cshtml (RazorViewEngine) 後找 .aspx (WebFormViewEngine)
+            //20170812 - 修正 , 不找 .aspx (WebFormViewEngine) 了 , 用不到
+            viewEngines.Clear();
+            viewEngines.Add(new CSharpRazorViewEngine());
+        }
+
+        //僅搜尋 cshtml (C# ) 檔 , vshtml (VB.NET) 檔 忽略
+        internal class CSharpRazorViewEngine : RazorViewEngine
+        {
+            public CSharpRazorViewEngine()
+            {
+                AreaViewLocationFormats = new[] {
+                    "~/Areas/{2}/Views/{1}/{0}.cshtml",
+                    "~/Areas/{2}/Views/Shared/{0}.cshtml"
+                };
+
+                AreaMasterLocationFormats = new[] {
+                    "~/Areas/{2}/Views/{1}/{0}.cshtml",
+                    "~/Areas/{2}/Views/Shared/{0}.cshtml"
+                };
+
+                AreaPartialViewLocationFormats = new[] {
+                    "~/Areas/{2}/Views/{1}/{0}.cshtml",
+                    "~/Areas/{2}/Views/Shared/{0}.cshtml"
+                };
+
+                ViewLocationFormats = new[] {
+                    "~/Views/{1}/{0}.cshtml",
+                    "~/Views/Shared/{0}.cshtml"
+                };
+
+                PartialViewLocationFormats = new[] {
+                    "~/Views/{1}/{0}.cshtml",
+                    "~/Views/Shared/{0}.cshtml"
+                };
+
+                MasterLocationFormats = new[] {
+                    "~/Views/{1}/{0}.cshtml",
+                    "~/Views/Shared/{0}.cshtml"
+                };
+
+                FileExtensions = new[] {
+                    "cshtml"
+                };
+            }
+        }
+
+    }
+}
--- digitallearningback/Global.asax.cs
+++ digitallearningback/Global.asax.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using digitallearningback.App_Start;
 
 namespace digitallearningback
 {
@@ -16,11 +17,7 @@ namespace digitallearningback
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
-
-            //調整ViewEngine 順序 先找.cshtml (RazorViewEngine) 後找 .aspx (WebFormViewEngine)
-            ViewEngines.Engines.Clear();
-            ViewEngines.Engines.Add(new RazorViewEngine());
-            ViewEngines.Engines.Add(new WebFormViewEngine());
+            ViewEngineConfig.Register(ViewEngines.Engines);
         }
     }
 }
