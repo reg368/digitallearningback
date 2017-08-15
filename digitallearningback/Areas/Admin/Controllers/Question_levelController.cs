ee8ebc516451b194e66c04b5315f5acfabcda08c
reg368 <reg368@gmail.com>
2017-08-15 22:23:04 +0800

    questionlevel add delete action

--- digitallearningback/Areas/Admin/Controllers/Question_levelController.cs
+++ digitallearningback/Areas/Admin/Controllers/Question_levelController.cs
@@ -16,7 +16,7 @@ namespace digitallearningback.Areas.Admin.Controllers
         private Question_levelService levelService = new Question_levelService();
 
         // GET: Admin/Question_level
-        public ActionResult Index(int id,String groupname)
+        public ActionResult Index(int id, String groupname)
         {
             ViewBag.group_id = id;
             ViewBag.groupname = groupname;
@@ -59,25 +59,27 @@ namespace digitallearningback.Areas.Admin.Controllers
             if (ModelState.IsValid)
             {
                 levelService.insert(question_level);
-                return RedirectToAction("Index",new { id = question_level.group_id , groupname = groupname });
+                return RedirectToAction("Index", new { id = question_level.group_id, groupname = groupname });
             }
             return View(question_level);
         }
 
-        /*
+
         // GET: Admin/Question_level/Edit/5
-        public ActionResult Edit(int? id)
+        public ActionResult Edit(int? id, String groupname)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Question_level question_level = db.Question_level.Find(id);
+            Question_level question_level = levelService.selectById(id);
             if (question_level == null)
             {
                 return HttpNotFound();
             }
-            ViewBag.group_id = new SelectList(db.Question_group, "id", "name", question_level.group_id);
+
+            ViewBag.groupname = groupname;
+
             return View(question_level);
         }
 
@@ -86,44 +88,69 @@ namespace digitallearningback.Areas.Admin.Controllers
         // 詳細資訊，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "id,level,joindate,group_id,isvisible,israndom,totalqnumber,correctqnumber,awardmoney,awardexperience,fromquestion,toquestion")] Question_level question_level)
+        public ActionResult Edit(
+            String groupname,
+            [Bind(Include = "id,level,isvisible,israndom,levelorder,correctqnumber,awardmoney,awardexperience")] Question_level question_level)
         {
-            if (ModelState.IsValid)
-            {
-                db.Entry(question_level).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            ViewBag.group_id = new SelectList(db.Question_group, "id", "name", question_level.group_id);
-            return View(question_level);
-        }
 
-        // GET: Admin/Question_level/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
+            Question_level record = levelService.selectById(question_level.id);
+
+            if (record == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Question_level question_level = db.Question_level.Find(id);
-            if (question_level == null)
+
+            if (ModelState.IsValid)
             {
-                return HttpNotFound();
+
+                record.level = question_level.level;
+                record.isvisible = question_level.isvisible;
+                record.israndom = question_level.israndom;
+                record.levelorder = question_level.levelorder;
+                record.correctqnumber = question_level.correctqnumber;
+                record.awardmoney = question_level.awardmoney;
+                record.awardexperience = question_level.awardexperience;
+
+                levelService.update(record);
+
+                return RedirectToAction("Index", new { id = record.group_id, groupname = groupname });
             }
+
+
             return View(question_level);
         }
 
-        // POST: Admin/Question_level/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            Question_level question_level = db.Question_level.Find(id);
-            db.Question_level.Remove(question_level);
-            db.SaveChanges();
-            return RedirectToAction("Index");
-        }
-        */
+        
+       // GET: Admin/Question_level/Delete/5
+       public ActionResult Delete(int? id, String groupname)
+       {
+           if (id == null)
+           {
+               return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+           }
+
+           Question_level question_level = levelService.selectById(id);
+
+            if (question_level == null)
+           {
+               return HttpNotFound();
+           }
+
+            ViewBag.groupname = groupname;
+            ViewBag.group_id = question_level.group_id;
+
+            return View(question_level);
+       }
+
+       // POST: Admin/Question_level/Delete/5
+       [HttpPost, ActionName("Delete")]
+       [ValidateAntiForgeryToken]
+       public ActionResult DeleteConfirmed(int id,int group_id, String groupname)
+       {
+           levelService.deleteByPrimaryKey(id);
+           return RedirectToAction("Index", new { id = group_id, groupname = groupname });
+       }
+       
         protected override void Dispose(bool disposing)
         {
             if (disposing)
