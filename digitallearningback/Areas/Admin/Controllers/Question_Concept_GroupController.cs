4d769fbbd45a12f7ab907315586ab39f60a16d58
reg368 <reg368@gmail.com>
2017-08-25 22:36:36 +0800

    add question concept group

---
+++ digitallearningback/Areas/Admin/Controllers/Question_Concept_GroupController.cs
@@ -0,0 +1,127 @@
+﻿using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+using digitallearningback.DAO;
+using digitallearningback.Models;
+using digitallearningback.Util;
+
+namespace digitallearningback.Areas.Admin.Controllers
+{
+    public class Question_Concept_GroupController : Controller
+    {
+        private InfoUser infoUser = SessionHelper.getLoginUser();
+        private Question_Concept_GroupService service = new Question_Concept_GroupService();
+
+        // GET: Admin/Question_Concept_Group
+        public ActionResult Index()
+        {
+            return View(service.selectListByUserid(infoUser.id));
+        }
+
+        // GET: Admin/Question_Concept_Group/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Question_Concept_Group question_Concept_Group = service.selectById(id);
+            if (question_Concept_Group == null)
+            {
+                return HttpNotFound();
+            }
+            return View(question_Concept_Group);
+        }
+
+        // GET: Admin/Question_Concept_Group/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/Question_Concept_Group/Create
+        // 若要免於過量張貼攻擊，請啟用想要繫結的特定屬性，如需
+        // 詳細資訊，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,name")] Question_Concept_Group question_Concept_Group)
+        {
+            if (ModelState.IsValid)
+            {
+                question_Concept_Group.user_id = infoUser.id;
+                service.insert(question_Concept_Group);
+                return RedirectToAction("Index");
+            }
+
+            return View(question_Concept_Group);
+        }
+
+        // GET: Admin/Question_Concept_Group/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Question_Concept_Group question_Concept_Group = service.selectById(id);
+            if (question_Concept_Group == null)
+            {
+                return HttpNotFound();
+            }
+            return View(question_Concept_Group);
+        }
+
+        // POST: Admin/Question_Concept_Group/Edit/5
+        // 若要免於過量張貼攻擊，請啟用想要繫結的特定屬性，如需
+        // 詳細資訊，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,name")] Question_Concept_Group question_Concept_Group)
+        {
+            if (ModelState.IsValid)
+            {
+                Question_Concept_Group record = service.selectById(question_Concept_Group.id);
+                record.name = question_Concept_Group.name;
+
+                service.update(record);
+
+                return RedirectToAction("Index");
+            }
+            return View(question_Concept_Group);
+        }
+
+        // GET: Admin/Question_Concept_Group/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Question_Concept_Group question_Concept_Group = service.selectById(id);
+            if (question_Concept_Group == null)
+            {
+                return HttpNotFound();
+            }
+            return View(question_Concept_Group);
+        }
+
+        // POST: Admin/Question_Concept_Group/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            service.deleteByPrimaryKey(id);
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                service.disposing();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ digitallearningback/DAO/Question_Concept_GroupService.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using digitallearningback.Models;
+
+namespace digitallearningback.DAO
+{
+    public class Question_Concept_GroupService
+    {
+        private yzucsEntities db = new yzucsEntities();
+
+        public void disposing()
+        {
+            if (db != null)
+            {
+                db.Dispose();
+            }
+        }
+
+        public int insert(Question_Concept_Group model)
+        {
+            db.Question_Concept_Group.Add(model);
+            return db.SaveChanges();
+        }
+
+        public int update(Question_Concept_Group record)
+        {
+            db.Entry(record).State = EntityState.Modified;
+            return db.SaveChanges();
+        }
+
+        public int deleteByPrimaryKey(int pk)
+        {
+            Question_Concept_Group record = db.Question_Concept_Group.Find(pk);
+            db.Question_Concept_Group.Remove(record);
+            return db.SaveChanges();
+        }
+
+        public Question_Concept_Group selectById(int? id)
+        {
+            return db.Question_Concept_Group.Find(id);
+        }
+
+        public List<Question_Concept_Group> selectListByUserid(int userid) {
+            var linq = db.Question_Concept_Group.Where(g => g.user_id == userid);
+            return linq.ToList<Question_Concept_Group>();
+        }
+    }
+}
