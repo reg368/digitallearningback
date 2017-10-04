828938997dcbada4886120ff571e43ca07cb2f55
reg368 <reg368@gmail.com>
2017-10-04 11:24:35 +0800

    add GameOn.cshtml error page

--- digitallearningback/Areas/Game/Controllers/ModelViewController.cs
+++ digitallearningback/Areas/Game/Controllers/ModelViewController.cs
@@ -10,12 +10,20 @@ namespace digitallearningback.Areas.Game.Controllers
         private QuestionService qservice = new QuestionService(); // 找 課程/關卡 題目
 
         // GET: Game/ModelView/QuestionDetail
+        //答案選擇的畫面
         public ActionResult QuestionDetail(int qid)
         {
             Question question = qservice.selectByIdIncludeAnswer(qid);
             return View(question);
         }
 
-
+        // GET: Game/ModelView/QuestionDetailInfo
+        //選錯了 跳出正確答案訊息的畫面
+        public ActionResult QuestionDetailInfo(int qid,int selectaid)
+        {
+            Question question = qservice.selectByIdIncludeAnswer(qid);
+            ViewBag.selectaid = selectaid;
+            return View(question);
+        }
     }
 }
