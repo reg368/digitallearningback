23d2ab895ebd8ef0f6615c8fe6a25f0efa5ef493
reg368 <reg368@gmail.com>
2017-11-05 20:25:22 +0800

    admin question conecpt add done

--- digitallearningback/Areas/Admin/Controllers/Question_Concept_MappingController.cs
+++ digitallearningback/Areas/Admin/Controllers/Question_Concept_MappingController.cs
@@ -1,4 +1,5 @@
 ﻿using digitallearningback.DAO;
+using digitallearningback.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,6 +14,7 @@ namespace digitallearningback.Areas.Admin.Controllers
         private VW_QuestionConceptDetailService vw_service = new VW_QuestionConceptDetailService();
         private QuestionService questionservice = new QuestionService();
         private Question_ConceptService conceptservice = new Question_ConceptService();
+        private Question_Concept_MappingService qcmservice = new Question_Concept_MappingService();
 
         // GET: Admin/Question_Concept_Mapping
         public ActionResult Index(int? qid)
@@ -29,10 +31,49 @@ namespace digitallearningback.Areas.Admin.Controllers
         }
 
         // GET: Admin/Question_Concept_Mapping
-        public ActionResult Create(int qid, String qtext)
+        public ActionResult AddConcept(int qid)
         {
             ViewBag.conceptlist = conceptservice.selectListByWhereNotInQid(qid);
             return View(questionservice.selectById(qid));
         }
+
+        // GET: Admin/Question_Concept_Mapping
+        public ActionResult DoAddConcept(int? qid,IEnumerable<String> cids)
+        {
+
+            if (qid == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            if (cids == null)
+            {
+                ModelState.AddModelError("error", "請選擇欲加入至題目的概念");
+                return RedirectToAction("AddConcept", new { qid = qid });
+            }
+
+            List<Question_Concept_Mapping> models = new List<Question_Concept_Mapping>();
+
+            foreach (String cid in cids)
+            {
+                Question_Concept_Mapping model = new Question_Concept_Mapping();
+                model.c_id = Convert.ToInt32(cid); ;
+                model.q_id = qid;
+                model.percentage = 0;
+                models.Add(model);
+            }
+
+            if (models.Count > 0)
+            {
+                qcmservice.insert(models);
+                return RedirectToAction("Index", new { qid = qid });
+            }
+            else
+            {
+                ModelState.AddModelError("error", "請選擇欲加入至題目的概念");
+                return RedirectToAction("AddQuestion", new { qid = qid });
+            }
+        }
+
     }
 }
---
+++ digitallearningback/DAO/Question_Concept_MappingService.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using digitallearningback.Models;
+
+namespace digitallearningback.DAO
+{
+    public class Question_Concept_MappingService
+    {
+        private yzucsEntities db = new yzucsEntities();
+
+        public void disposing()
+        {
+            if (db != null)
+            {
+                db.Dispose();
+            }
+        }
+
+        public int insert(List<Question_Concept_Mapping> models)
+        {
+            db.Question_Concept_Mapping.AddRange(models);
+            return db.SaveChanges();
+        }
+    }
+}
