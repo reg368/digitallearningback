27a4d5d1f52035fa37d02e77c0eaa5e61037c87a
reg368 <reg368@gmail.com>
2018-03-31 18:13:19 +0800

    add vw_DataAnalysisLevelDetailInfo service

---
+++ digitallearningback/DAO/VW_DataAnalysisLevelDetailInfoService.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using digitallearningback.Models;
+using System.Data.Entity;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+
+
+namespace digitallearningback.DAO
+{
+    public class VW_DataAnalysisLevelDetailInfoService
+    {
+        private yzucsEntities db = new yzucsEntities();
+
+        public void disposing()
+        {
+            if (db != null)
+            {
+                db.Dispose();
+            }
+        }
+
+        public vw_DataAnalysisLevelDetailInfo selectByLevelId(int lid)
+        {
+            return db.vw_DataAnalysisLevelDetailInfo.Where(d => d.lid == lid).FirstOrDefault<vw_DataAnalysisLevelDetailInfo>();
+        }
+    }
+}
