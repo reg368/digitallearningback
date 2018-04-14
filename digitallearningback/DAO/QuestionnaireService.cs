425d9f9093de3607524c25a38fbb8537cf7cb585
reg368 <reg368@gmail.com>
2018-04-14 13:43:42 +0800

    add questionnaire service method

--- digitallearningback/DAO/QuestionnaireService.cs
+++ digitallearningback/DAO/QuestionnaireService.cs
@@ -26,5 +26,25 @@ namespace digitallearningback.DAO
             return result;
         }
 
+        public int selectCountForQuestionnaire_main()
+        {
+            var result = db.Database.SqlQuery<int>(
+               "select count(*) from Questionnaire_main").FirstOrDefault();
+            return result;
+        }
+
+        public Questionnaire_main selectQuestionnaire_mainByShow_order(int show_order) {
+            var query = db.Questionnaire_main.SqlQuery(
+                "Select * from Questionnaire_main where show_order = @show_order",
+                new SqlParameter("@show_order", show_order));
+            return query.FirstOrDefault<Questionnaire_main>();
+        }
+
+        public List<Questionnaire_option> selectQuestionnaire_optionByMain_id(int main_id) {
+            var query = db.Questionnaire_option.SqlQuery(
+               "Select * from Questionnaire_option where main_id = @main_id",
+               new SqlParameter("@main_id", main_id));
+            return query.ToList<Questionnaire_option>();
+        }
     }
 }
